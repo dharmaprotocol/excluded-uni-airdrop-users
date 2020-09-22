86aac39dbd17d9a6fbf2ae16143741b1c3f99558
Graeme Boy <graemelearn@protonmail.com>
2020-09-22 16:07:17 -0700

    add test

--- test.js
+++ test.js
@@ -3,7 +3,11 @@ const fs = require('fs');
 const { exec } = require("child_process");
 const zlib = require('zlib');
 
-describe("Projects", () => {
+// There is one single test here that takes a lot of time,
+// so we must set a high timeout threshold.
+jest.setTimeout(1000000);
+
+describe("Given all of the known accounts to interact with Uniswap", () => {
     let validAccounts;
 
     beforeAll(async () => {
@@ -17,11 +21,22 @@ describe("Projects", () => {
         fileContents.pipe(unzip).pipe(writeStream);
     });
 
-    describe("Dharma", () => {
-        let accounts;
+    describe("and all of the proposed accounts from projects in this repo", () => {
+        test("every proposed account is also in the set of known accounts", async () => {
+            const scriptOutput = await new Promise((accept, reject) => {
+                exec("./scripts/compare_addresses", (err, stdout, stderr) => {
+                    if (err) {
+                        reject(stderr);
+                    }
 
-        it("every submitted address is in the list of valid addresses", async () => {
-            // TODO Compare each line in project with valid-accounts.txt.
+                    accept(stdout);
+                });
+            });
+
+            const invalidAccounts = scriptOutput.trim().split("\n");
+
+            // If there is no output, then every line matched valid accounts.
+            expect(invalidAccounts).toEqual([]);
         });
     });
 });
